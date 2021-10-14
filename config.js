995df716d71bf78993b897c16325b0789424e4cd
ngocduc202 <ngocduc1542002@gmail.com>
2021-10-14 19:44:43 +0700

    new file

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+  introTitle: "Babe à!",
+  introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi`,
+  btnIntro: "hihi!!",
+  title: "Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙",
+  desc: "Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ",
+  btnYes: "Có nè  <33",
+  btnNo: "Không, Anh trai à :3",
+  question: "Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3",
+  btnReply: "Gửi ik nào <3",
+  reply: "Em yêu anh <33333333",
+  mess: "Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘",
+  messDesc: ":33333333",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://www.messenger.com/t/100033881108922/",
+};
